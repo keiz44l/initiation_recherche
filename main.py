c12003bb79c8570033a827789d9ea63ea6e7a726
Pierre LEFLAEC <pierre.leflaec67@gmail.com>
2024-05-07 11:35:15 +0200

    change path

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 
 def main():
     instances = []
     instance = {}
     # Read the data
-    with open("bbob-biobj_f01_d02_hyp.tdat", "r") as file:
+    with open("./RM-MEDA_Auger_bbob-biobj/d02_d03_d10/1-separable_1-separable/bbob-biobj_f01_d02_hyp.tdat", "r") as file:
         for line in file:
             if line[0] == "%" and instance:
                 instances.append(instance)
@@ -14,9 +12,16 @@ def main():
             if line[0] == "%":
                 continue
             line = line.split()
-            print(line)
+            instance[int(line[0])]=float(line[1])
+        instances.append(instance) 
+    print(instances)
 
+    myList = instances[0].items()
+    myList = sorted(myList)
+    x, y = zip(*myList)
 
+    plt.plot(x, y)
+    plt.show()
 
 if __name__ == "__main__":
     main()
